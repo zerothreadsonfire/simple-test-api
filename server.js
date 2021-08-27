a76f0fb00cb8b1499359909df9059e7decbfa82d
satyam <satyamonsocial@gmail.com>
2021-08-27 16:52:15 +0530

    add starter files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const data = require('')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Test API hostend on Heroku');
+});
+
+app.get('/data', (req, res) => {
+    res.send(data);
+});
+
+app.listen(5000, ()=> {
+    console.log('listening on port 5000');
+})
